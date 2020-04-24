8021c6202f259607edd6a0d9cb80f4c6eda30389
srima24 <60697547+srima24@users.noreply.github.com>
2020-04-24 20:47:13 +0530

    updatde

--- vars/sayhello.groovy
+++ vars/sayhello.groovy
@@ -1,19 +1,19 @@
 #!/usr/bin/env groovy
 
 set +x
-
+info() {
+echo "\033[1;33m[Info]    \033[0m $1"
+}
 
 error() {
-echo "\033[1;31m [Error]   \033[0m "
+echo "\033[1;31m[Error]   \033[0m $1"
 }
 
 
 success() {
-echo "\033[1;32m [Success] \033[0m "
+echo "\033[1;32m[Success] \033[0m $1"
 }
 
-
-def call(String str = "hello") {
-echo "\033[1;31m [Error] hello   \033[0m "
-echo "\033[1;32m [Success] hello \033[0m "
+def call(String name = 'ansi color') {
+  echo "${name}"
 }
